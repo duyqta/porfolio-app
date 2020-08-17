71c7d511f55114e600e9b424aad4be49cbc4bf22
Tony Ta <quangcic3737@gmail.com>
2020-08-17 00:33:32 -0400

    Updates

---
+++ precache-manifest.08dfc2303b4f80acf6c991b80706475e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b345ab129e1c8567e254ee906b87368",
+    "url": "/porfolio-app/index.html"
+  },
+  {
+    "revision": "ced18a19326adb0c188c",
+    "url": "/porfolio-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "337938e2a65f78a8efba",
+    "url": "/porfolio-app/static/js/2.d564d046.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/porfolio-app/static/js/2.d564d046.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ced18a19326adb0c188c",
+    "url": "/porfolio-app/static/js/main.b2bd0f46.chunk.js"
+  },
+  {
+    "revision": "0449c165c802f66f8eb4",
+    "url": "/porfolio-app/static/js/runtime-main.6086c9b2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/porfolio-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
